43bc1bc4e097d9af2a100107f67b6279b41d0056
xiul <xiul10901@gmail.com>
2017-01-27 12:01:30 +0100

    Finished basic clustering script

--- 2-Clustering.py
+++ 2-Clustering.py
@@ -23,9 +23,7 @@ print("Clusters "+str(nClusters))
 
 clusters=[all_data[all_data['cluster'].isin([i])] for i in range(nClusters+1)]
 
-print(clusters[0])
-
-df=pd.DataFrame(columns=('cluster','ncars','meanPowerPS','diferentModels','diferentBrand','diferentCarType','monthsMean','priceMean','kilometerMean'))
+df=pd.DataFrame(columns=('cluster','ncars','meanPowerPS','diferentModels','diferentBrand','monthsMean','priceMean','kilometerMean','nBenzin','nCng','nDiesel','nHybrid','nElektro','nLpg','nAndere','nBus','nCabrio','nCoupe','nKleinwagen','nSuv','nKombi','nLimousine'))
 
 for i in range(nClusters+1):
 	c=clusters[i]
@@ -33,9 +31,24 @@ for i in range(nClusters+1):
 	meanPowerPS=c['powerPS'].mean()
 	diferentModels=len(c['model'].unique())
 	diferentBrand=len(c['brand'].unique())
-	diferentCarType=len(c['vehicleType'].unique())
 	monthsMean=c['antiguedad'].mean()
 	priceMean=c['price'].mean()
 	kilometerMean=c['kilometer'].mean()
-	df.loc[i]=[i,count,meanPowerPS,diferentModels,diferentBrand,diferentCarType,monthsMean,priceMean,kilometerMean]
-print (df)
+	#FuelTypes
+	types=c.groupby('fuelType').size()
+	def getN(fType):
+		try:
+			return types[fType]
+		except KeyError:
+			return 0
+	nFuelTypes=[getN('benzin'),getN('cng'),getN('diesel'),getN('hybrid'),getN('elektro'),getN('lpg')]
+	#VehicleTypes
+	types=c.groupby('vehicleType').size()
+	nVehicleTypes=[getN('andere'),getN('bus'),getN('cabrio'),getN('coupe'),getN('kleinwagen'),getN('suv'),getN('kombi'),getN('limousine')]
+	
+	df.loc[i]=[i,count,meanPowerPS,diferentModels,diferentBrand,monthsMean,priceMean,kilometerMean]+nFuelTypes+nVehicleTypes
+csv_file=df.to_csv(index=False)
+
+fileout=open('clusters.csv','w')
+fileout.write(csv_file)
+fileout.close()
