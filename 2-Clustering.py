9397c3db4adf13f6ee0a6e619b2beb4c4990bded
xiul <xiul10901@gmail.com>
2017-01-29 13:22:11 +0100

    Added comments and modified hardcoded stuff

--- 2-Clustering.py
+++ 2-Clustering.py
@@ -1,29 +1,44 @@
+#imports
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
+import sys
 from sklearn.cluster import DBSCAN
 from sklearn import preprocessing
 from sklearn import metrics
 
-all_data = pd.read_csv('super_clean.csv', sep=',',encoding='Latin1')
+#Arguments management
+if(len(sys.argv) is not 3):
+	print("Usage: python3 E3.py <input_file>  <output_file>")
+	exit()
 
+#File opening
+all_data = pd.read_csv(sys.argv[1], sep=',',encoding='Latin1')
+
+#Printing data size
 print(all_data['price'].size)
 
+#Extracting the elements to use for the clustering
 numpyData=np.array([all_data['price'],all_data['kilometer'],all_data['antiguedad']]).T
 
+#Normalizing data
 min_max_scaler = preprocessing.MinMaxScaler()
 numpyData = min_max_scaler.fit_transform(numpyData)
 
+#Clustering
 db=DBSCAN(eps=0.015,min_samples=8,metric='euclidean')
 ydb=db.fit_predict(numpyData)
 all_data['cluster']=ydb
 nClusters=max(ydb)
 print("Clusters "+str(nClusters))
 
+
+#Spliting elements of each cluster in diferents dataFrames
 clusters=[all_data[all_data['cluster'].isin([i])] for i in range(nClusters+1)]
 
-df=pd.DataFrame(columns=('cluster','ncars','meanPowerPS','diferentModels','diferentBrand','monthsMean','priceMean','kilometerMean','nBrokenCars','nBenzin','nCng','nDiesel','nHybrid','nElektro','nLpg','nAndere','nBus','nCabrio','nCoupe','nKleinwagen','nSuv','nKombi','nLimousine'))
+#Defining new features for the clusters
+df=pd.DataFrame(columns=('cluster', 'ncars', 'meanPowerPS', 'diferentModels', 'diferentBrand', 'monthsMean', 'priceMean', 'kilometerMean', 'nBrokenCars', 'nBenzin', 'nCng', 'nDiesel', 'nHybrid', 'nElektro', 'nLpg', 'nAndere', 'nBus', 'nCabrio', 'nCoupe', 'nKleinwagen', 'nSuv', 'nKombi', 'nLimousine'))
 
 for i in range(nClusters+1):
 	c=clusters[i]
@@ -53,6 +68,7 @@ for i in range(nClusters+1):
 	df.loc[i]=[i,count,meanPowerPS,diferentModels,diferentBrand,monthsMean,priceMean,kilometerMean,nBrokenCars]+nFuelTypes+nVehicleTypes
 csv_file=df.to_csv(index=False)
 
-fileout=open('clusters.csv','w')
+#Writting the new file
+fileout=open(sys.argv[2],'w')
 fileout.write(csv_file)
 fileout.close()
