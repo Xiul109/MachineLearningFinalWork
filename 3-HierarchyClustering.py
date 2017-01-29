dfa673ac0cf1e8644d0f03e92f77252da1fc410e
Iván García <ciberpatro@gmail.com>
2017-01-29 13:38:46 +0100

    Added comments

--- 3-HierarchyClustering.py
+++ 3-HierarchyClustering.py
@@ -7,7 +7,7 @@ import sklearn.neighbors
 from scipy import cluster
 
 if(len(sys.argv) is not 3):
-	print("Usage: python3 E3.py <input_file>  <output_file>")
+	print("Usage: python3 3-HierarchyClustering.py <input_file> <output_file>")
 	exit()
 csv_file=csv.reader(open(sys.argv[1]))
 aux=list(csv_file)
@@ -19,9 +19,6 @@ numpyData=list(map(lambda x : list(map(float,x[1:])), aux[1:]))
 min_max_scaler = preprocessing.MinMaxScaler()
 numpyData = min_max_scaler.fit_transform(numpyData)
 
-
-#metrics=['euclidean', 'manhattan', 'chebyshev','minkowski','wminkowski','seuclidean','mahalanobis']
-#methods=['complete', 'average', 'weighted','single','ward','median','centroid']
 metric='euclidean'
 method='complete'
 threshold=5.5
@@ -33,7 +30,6 @@ avSim = np.average(matsim)
 clusters = cluster.hierarchy.linkage(matsim, method = method)
 cluster.hierarchy.dendrogram(clusters, color_threshold=threshold)
 plt.title(metric+" "+method)
-#plt.savefig(me+" "+m, dpi=1000)
 plt.show()
 
 clusters=cluster.hierarchy.fcluster(clusters,threshold,criterion='distance')
@@ -64,6 +60,7 @@ for i in range(min(clusters),max(clusters)+1):
 			broken+=float(d[colDamages])*float(d[colNCars])
 	print(i,price/count,km/count,months/count, broken/ncars)
 
+#Writing file
 data.insert(0,fields+['gama'])
 with open(sys.argv[2],'w') as f:
 	writer=csv.writer(f,fields+['gama'])
